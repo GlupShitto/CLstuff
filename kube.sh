a7c449fe5bff71b8175f27a84eff56be843a5b7e
GlupShitto <108697351+GlupShitto@users.noreply.github.com>
2022-12-03 18:57:27 +0100

    Update kube.sh

--- kube.sh
+++ kube.sh
@@ -3,24 +3,24 @@
 wget -P /etc/yum.repos.d/ https://download.docker.com/linux/centos/docker-ce.repo
 wget -P /etc/yum.repos.d/ https://raw.githubusercontent.com/GlupShitto/kubernetes/main/kubernetes.repo
 
-sleep 5
+
 
 sed -i '22s/enforcing/disabled/' /etc/selinux/config
 systemctl disable firewalld
 
-sleep 5
+
 
 cat > /etc/modules-load.d/containerd.conf <<EOF
 overlay
 br_netfilter
 EOF
 
-sleep 5
+
 
 modprobe overlay
 modprobe br_netfilter
 
-sleep 5
+
 
 cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
 net.bridge.bridge-nf-call-iptables  = 1
@@ -28,16 +28,16 @@ net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
-sleep 5
+
 
 sysctl --system
 
-sleep 5
+
 dnf install -y  yum-utils device-mapper-persistent-data lvm2
 
 dnf update -y && dnf install -y containerd.io
 
-sleep 5
+
 mkdir -p /etc/containerd
 
 containerd config default > /etc/containerd/config.toml
@@ -47,11 +47,11 @@ systemctl restart containerd
 
 systemctl enable containerd
 
-sleep 5
+
 
 dnf install -y kubeadm kubelet kubectl
 
-sleep 5
+
 
 systemctl enable kubelet
 echo 'KUBELET_EXTRA_ARGS="--fail-swap-on=false"' > /etc/sysconfig/kubelet
